f92a6ef43f4fdd6e2c513779699efeead162e640
takoji3 <takoji3g@gmail.com>
2014-05-04 19:59:33 +0900

    Fix indent

--- src/CsvChecker.php
+++ src/CsvChecker.php
@@ -2,7 +2,7 @@
 
 class CsvCheckException extends RuntimeException 
 {
-public function __construct($message = '', $file = '', $check = '')
+    public function __construct($message = '', $file = '', $check = '')
     {
         $msg = '';
         if ($file != '') {
